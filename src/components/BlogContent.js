65cb38eb30b3692df44522cf7be519ee21a2cc48
SheeTabz <wmuriithitabitha@gmail.com>
2022-09-20 15:37:20 +0300

    Completed props practice

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 function BlogContent(props) {
+  if(!props.isPublished) return null;
+  else{
   return <div id="blog-content">{props.articleText}</div>;
-}
+}}
 
 export default BlogContent;
